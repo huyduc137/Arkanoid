8ce18dc8550977e4aa2a2d547eba4972813963f4
Pham Tat Dat <convitduc6@gmail.com>
2025-10-01 17:51:37 +0700

    feat: Tạo class UIButton và UILable (nút bấm và text)

---
+++ src/game/view/UI/UILabel.java
@@ -0,0 +1,29 @@
+package game.view.UI;
+
+import java.awt.*;
+import java.util.function.Supplier;
+
+public class UILabel extends UIElement {
+    //Text supplier là functional interface -> cho phép truyền một lamda function để được gói vào thành object
+    //gọi get() mỗi khi được render để update text một cách tự động
+    private final Supplier<String> textSupplier;
+    private final Color color;
+    private final Font font;
+
+    public UILabel(int x, int y, Supplier<String> textSupplier, Color color, Font font) {
+        super(x, y, 0, 0);
+        this.textSupplier = textSupplier;
+        this.color = color;
+        this.font = font;
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setFont(font);
+        g.setColor(color);
+        g.drawString(textSupplier.get(), x, y);
+    }
+
+    @Override
+    public void onClick() {} //text ko cho click
+}
