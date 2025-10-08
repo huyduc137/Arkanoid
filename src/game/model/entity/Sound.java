889ae8608502063bbedde829088a6df57a34f341
Pham Tat Dat <convitduc6@gmail.com>
2025-10-09 00:23:30 +0700

    feat: tạo Sound class
    - Chứa clip là file audio + type để tuỳ chỉnh tách hiệu ứng và nhạc + âm lượng

---
+++ src/game/model/entity/Sound.java
@@ -0,0 +1,33 @@
+package game.model.entity;
+
+import javax.sound.sampled.Clip;
+
+public class Sound {
+    public enum Type {
+        SFX,
+        MUSIC
+    }
+
+    private final Clip clip;
+    private final Type type;
+    private final float baseVolume;
+
+    public Sound(Clip clip, Type type, float baseVolume) {
+        this.clip = clip;
+        this.type = type;
+        this.baseVolume = baseVolume;
+    }
+
+    public Clip getClip() {
+        return clip;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public float getBaseVolume() {
+        return baseVolume;
+    }
+}
+
