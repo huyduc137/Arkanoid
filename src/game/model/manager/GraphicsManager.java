be6cba89d6bd24bbd29cfa21782baf0545099390
Pham Tat Dat <convitduc6@gmail.com>
2025-10-11 15:45:38 +0700

    feat: tạo GraphicsManager để là nguồn load và cung cấp sprites
    - Load hết đầu game cho tiện

---
+++ src/game/model/manager/GraphicsManager.java
@@ -0,0 +1,45 @@
+package game.model.manager;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GraphicsManager {
+    private static final Map<String, BufferedImage> sprites = new HashMap<>();
+
+    private GraphicsManager() {}
+
+    public static void loadAll() {
+        loadSprite("ball", "sprites/ball/ball.png");
+
+        loadSprite("brick_1", "sprites/bricks/brick_1.png");
+        loadSprite("brick_2", "sprites/bricks/brick_2.png");
+        loadSprite("brick_3", "sprites/bricks/brick_3.png");
+        loadSprite("brick_unbreakable", "sprites/bricks/brick_unbreakable.png");
+
+        loadSprite("button_play", "sprites/buttons/play.png");
+
+        loadSprite("paddle", "sprites/paddle/paddle.png");
+    }
+
+    public static void loadSprite(String id, String path) {
+        try (InputStream is = GraphicsManager.class.getResourceAsStream("/" + path)) {
+            if (is == null) {
+                System.err.println("Sprite not found: " + path);
+                return;
+            }
+
+            BufferedImage img = ImageIO.read(is);
+            sprites.put(id, img);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static BufferedImage getSprite(String id) {
+        return sprites.get(id);
+    }
+}
