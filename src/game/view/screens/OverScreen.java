d402fff7893ee35bba758b555901f44e06bed76b
huyduc <huynguyen26nt@gmail.com>
2025-10-17 16:22:11 +0700

    thêm điểm cho màn thua

--- src/game/view/screens/OverScreen.java
+++ src/game/view/screens/OverScreen.java
@@ -2,10 +2,14 @@ package game.view.screens;
 
 import game.Constants;
 import game.model.GameModel;
+import game.model.manager.FontManager;
 import game.model.manager.GameStateManager;
+import game.model.manager.HighScoreManager;
 import game.view.UI.UIButton;
+import game.view.UI.UILabel;
 
 import java.awt.*;
+import java.util.function.Supplier;
 
 import static game.Constants.BUTTON_HEIGHT;
 import static game.Constants.BUTTON_WIDTH;
@@ -39,5 +43,28 @@ public class OverScreen extends Screen {
                     model.getGameStateManager().setGameMenu();
                 }) //action chạy khi click vào nút
         );
+
+        String levelName = model.getLevelManager().getCurrentLevel().getName();
+        Font scoreFont = FontManager.getFont("Tektur Bold", 32f);
+        Color textColor = Color.WHITE;
+        uiManager.add(new UILabel(
+                400,
+                400,
+                ()->""+model.getScoreSystem().getScore(),
+                textColor,
+                scoreFont
+        ));
+        Supplier<String> scoreTextSupplier = () -> {
+            int highScore = HighScoreManager.getHighScore(levelName);
+            return "" + (highScore > 0 ? highScore : "0000");
+        };
+        uiManager.add(new UILabel(
+                550,
+                400,
+                scoreTextSupplier,
+                textColor,
+                scoreFont
+        ));
+
     }
 }
