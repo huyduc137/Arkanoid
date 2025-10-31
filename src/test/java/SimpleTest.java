a3ff5376dba768042408888e8c36f2b8062c4335
Pham Tat Dat <convitduc6@gmail.com>
2025-10-31 10:10:04 +0700

    feat: wrap project vao Maven va them JUnit 4.13

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+    @Test
+    public void additionShouldWork() {
+        assertEquals(4, 2 + 2);
+    }
+}
