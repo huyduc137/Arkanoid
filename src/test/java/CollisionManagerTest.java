b0e8b5738fe91893e7cb9f08d55ffd0864e4d246
Pham Tat Dat <convitduc6@gmail.com>
2025-11-02 09:45:02 +0700

    fix: CollisionManagerTest setUp
    - Clear brick GameModel để test riêng với brick được khởi tạo

--- src/test/java/CollisionManagerTest.java
+++ src/test/java/CollisionManagerTest.java
@@ -20,6 +20,9 @@ public class CollisionManagerTest {
     @Before
     public void setUp() {
         gameModel = new GameModel();
+        gameModel.getBricks().clear();
+        gameModel.getBalls().clear();
+
         collisionManager = new CollisionManager(gameModel);
         ball = new Ball(0, 0, Constants.BALL_DIAMETER);
         brick = new Brick(0, 0, Constants.BRICK_WIDTH, Constants.BRICK_HEIGHT, 1, Brick.BrickType.NORMAL);
@@ -37,7 +40,7 @@ public class CollisionManagerTest {
 
         // set vị trí ball
         ball.setX(100 + (Constants.BRICK_WIDTH / 2) - (Constants.BALL_DIAMETER / 2));
-        ball.setY(100 -  (Constants.BALL_DIAMETER) + 20);
+        ball.setY(100 -  (Constants.BALL_DIAMETER));
 
         // set tốc độ ball
         ball.setDx(0);
@@ -49,20 +52,28 @@ public class CollisionManagerTest {
 
         System.out.println("Ball X: " + ball.getX() + " Ball Y: " + ball.getY());
         System.out.println("Brick X: " + brick.getX() + " Brick Y: " + brick.getY());
+        System.out.println("Fireball? " + ball.isFireBall());
         System.out.println("Giao nhau? " + ball.getBounds().intersects(brick.getBounds()));
         System.out.println("Cooldown: " + ball.getCollisionCooldown());
+        System.out.println("Ball Dy: " + ball.getDy());
+        System.out.println("Brick HP: " + brick.getHitPoints());
+
+        ball.move(0.016);
         collisionManager.checkCollisions();
 
         System.out.println("------------------------------------");
         System.out.println("Ball X: " + ball.getX() + " Ball Y: " + ball.getY());
         System.out.println("Brick X: " + brick.getX() + " Brick Y: " + brick.getY());
+        System.out.println("Fireball? " + ball.isFireBall());
         System.out.println("Giao nhau? " + ball.getBounds().intersects(brick.getBounds()));
         System.out.println("Cooldown: " + ball.getCollisionCooldown());
+        System.out.println("Ball Dy: " + ball.getDy());
+        System.out.println("Brick HP: " + brick.getHitPoints());
 
         // máu của brick giảm đi 1
         assertEquals("Máu của Brick phải giảm đi 1", brickHp - 1, brick.getHitPoints());
 
         // sau khi va chạm Dy của ball phải được đổi chiều
-//        assertEquals("sau khi va chạm Dy của ball phải được đổi chiều", -ballDy, ball.getDy(), 0.01);
+        assertEquals("sau khi va chạm Dy của ball phải được đổi chiều", -ballDy, ball.getDy(), 0.01);
     }
 }
